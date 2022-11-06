83237169b4c06f4a95bc86bca50d807ae79db310
Naman Biyani <99156947+NamanBiyani06@users.noreply.github.com>
2022-11-06 15:17:12 -0500

    Update GraphTheoryIntro.cpp

--- C++/Graphy_Theory_Intro/GraphTheoryIntro.cpp
+++ C++/Graphy_Theory_Intro/GraphTheoryIntro.cpp
@@ -7,41 +7,31 @@ int main()
 {
   // n is number of nodes
   // m is number of edges
-  int n, m;
-  cin >> n >> m;
+  int n, m; cin >> n >> m;
 
   // array of size n containing integer vectors
   //array: type vector, size n
   vector<int> graph[n];
 
-  
-  for (int i = 0; i < m; i++)
-  {
+  for (int i = 0; i < m; i++){
     // input consists of 2 ints per line
     // each line has two nodes which a connected bidirectionally
-    int n1, n2;
-    cin >> n1 >> n2;
-    n1--;
-    n2--;
-    if(n1 != n2)
-    {
+    int n1, n2; cin >> n1 >> n2; n1--; n2--;
+
+    if(n1 != n2){
       graph[n1].push_back(n2);
       graph[n2].push_back(n1); 
     }
-    else
-    {
-      graph[n1].push_back(n2);
-    }
+    else graph[n1].push_back(n2);
   }
   
   for(int i = 0; i < n; i++)
   {
+    //Sort because of problem specifications
     sort(graph[i].begin(), graph[i].end());
-    for(int j = 0; j < graph[i].size(); j++)
-    {
+    for(int j = 0; j < graph[i].size(); j++){
       cout << ((graph[i]).at(j) + 1) << " ";
     }
     cout << "\n";
   }
-  
 }
